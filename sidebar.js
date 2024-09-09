e5d74e7b42acefbdf32af85acc528ec92e17d5e1
Uralbek-DOM <uralbekdom@gmail.com>
2024-09-09 09:37:28 +0500

    update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,4 @@
-document.getElementById("sidebar").innerHTML = "Uralbek UZ";
+document.getElementById("sidebar0").innerHTML = "";
 document.getElementById("logotip").innerHTML = "Uralbek UZ";
 document.getElementById("logotip").innerHTML = "Uralbek UZ";
 document.getElementById("logotip").innerHTML = "Uralbek UZ";
